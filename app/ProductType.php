40ed3aadad80686036477bb380ae4e86d123e9f1
dat78787878 <d782000@gmail.com>
2021-03-01 12:57:02 +0700

    Web-ban-banh

---
+++ app/ProductType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductType extends Model
+{
+    //
+    protected $table = "type_products";
+
+    public function product(){
+        return $this->hasMany('App\Product','id_type','id');
+    }
+
+    public function bill_details(){
+         return $this->hasMany('App\BillDetail','id_product','id');
+    }
+}
